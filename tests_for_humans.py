1934d266ddba1e1189460ccc9b8ac45bb7523994
Chris “Kwpolska” Warrick <kwpolska@gmail.com>
2013-10-05 10:42:08 +0200

    Tests for Humans
    Signed-off-by: Chris “Kwpolska” Warrick <kwpolska@gmail.com>

---
+++ tests_for_humans.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# River of Text — tests for humans
+# Copyright © 2013, Kwpolska.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions, and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions, and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the author of this software nor the names of
+#    contributors to this software may be used to endorse or promote
+#    products derived from this software without specific prior written
+#    consent.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import rot.progress
+import rot.throbber
+import time
+
+print('River of Text — tests for humans\n')
+
+print('rot.progress.FrontProgress')
+p = rot.progress.FrontProgress(2, 'construct')
+time.sleep(0.5)
+p.step('1step')
+time.sleep(0.5)
+p.step('Done')
+
+
+print('rot.progress.FrontProgressThrobber')
+with rot.progress.FrontProgressThrobber(2, 'construct') as p:
+    time.sleep(0.5)
+    p.step('1step')
+    time.sleep(0.5)
+    p.step('Done')
+
+print('rot.throbber.Throbber')
+with rot.throbber.Throbber('a') as t:
+    time.sleep(0.5)
+    t.msg = 'hello'
+    time.sleep(0.5)
